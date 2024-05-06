32749d1b9c212e148f05b41f8f2064659547b402
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-05-06 13:58:02 +1200

    Implement printing of 'Start Round #x:' with incrementing x, and prompt for input

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,6 +5,7 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  int numOfRounds = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
@@ -13,7 +14,15 @@ public class Game {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    // Print "Start round: #x:" where x increases by 1 each time play is invoked.
+    ++numOfRounds;
+    MessageCli.START_ROUND.printMessage(Integer.toString(numOfRounds));
+
+    // Ask user for input on number of fingers.
+    MessageCli.ASK_INPUT.printMessage();
+
+  }
 
   public void endGame() {}
 
