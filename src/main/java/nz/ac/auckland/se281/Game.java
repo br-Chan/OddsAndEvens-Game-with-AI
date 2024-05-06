cd86fab50aecf13380f1c04260274629f6048f88
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2024-05-06 17:29:51 +1200

    Implement printing of information about player name and number of fingers after valid input in play method in Game class

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,13 +5,15 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  String playerName = null;
   int numOfRounds = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    playerName = options[0];
 
     // Print Welcome message.
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    MessageCli.WELCOME_PLAYER.printMessage(playerName);
   }
 
   public void play() {
@@ -20,18 +22,20 @@ public class Game {
     MessageCli.START_ROUND.printMessage(Integer.toString(numOfRounds));
 
     // Prompt user for input for number of fingers.
-    String input = null;
-    while (!Utils.isInteger(input) || Integer.parseInt(input) > 5 || Integer.parseInt(input) < 0) {
+    String numOfFingers = null;
+    while (!Utils.isInteger(numOfFingers) || Integer.parseInt(numOfFingers) > 5 || Integer.parseInt(numOfFingers) < 0) {
       // If user has input before, print the invalid input message.
-      if (input != null) {
+      if (numOfFingers != null) {
         MessageCli.INVALID_INPUT.printMessage();
       }
 
       // Print prompt message and get input.
       MessageCli.ASK_INPUT.printMessage();
-      input = Utils.scanner.nextLine();
+      numOfFingers = Utils.scanner.nextLine();
     }
 
+    // Print information about player name and number of fingers.
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, numOfFingers);
 
   }
 
